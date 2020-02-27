0e660b20c3a3189a003055460220f4b534cdf72f
Julius Pedersen <deifyed@gmail.com>
2020-02-27 08:11:02 +0100

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-	publicPath: process.env.NODE_ENV === 'production' ? '/hackathon-app-template/' : '/'
+	publicPath: process.env.NODE_ENV === 'production' ? '/tp-utviklerportalen/' : '/'
 }
