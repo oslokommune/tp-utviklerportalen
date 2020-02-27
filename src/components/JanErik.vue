142854a76c69103c48bc196fa2e910295c036b1d
Jan Erik Haavet <jan.haaavet@byr.oslo.kommune.no>
2020-02-27 11:37:51 +0100

    Added more text to list items

--- src/components/JanErik.vue
+++ src/components/JanErik.vue
@@ -8,7 +8,9 @@
 					</div>
 					<div class="list-item-text">
 						<h2>Badetempratur API</h2>
-						<p>Dette API-et lar deg hente de siste målingene av vanntempratur i Oslo.</p>
+						<p>Dette er Oslo kommunes badetempratur API. Med dette API-et kan du sjekke kommunens siste
+							vanntempraturmålinger fra badeplasser i Oslo området. Er du tøff nok til å la være å
+							sjekke tempraturen før du bader?</p>
 					</div>
 					<div class="list-item-button">
 						<BaseButton primary style="margin: 1em 0 1em 0;">Prøv</BaseButton>
@@ -22,7 +24,8 @@
 					</div>
 					<div class="list-item-text">
 						<h2>SMS API</h2>
-						<p>Dette API-et gir mulighet til å sende SMS fra Oslo kommune.</p>
+						<p>Dette API-et gir mulighet til å sende SMS fra Oslo kommune. Har du et behov for å sende
+							sms fra tjenesten din?</p>
 					</div>
 					<div class="list-item-button">
 						<BaseButton primary style="margin: 1em 0 1em 0;">Prøv</BaseButton>
@@ -36,7 +39,8 @@
 					</div>
 					<div class="list-item-text">
 						<h2>Epost API</h2>
-						<p>Dette API-et gir mulighet til å sende epost fra Oslo kommune.</p>
+						<p>Dette API-et gir mulighet til å sende epost fra Oslo kommune. Har du et behov for å sende
+							epost fra tjenesten din?</p>
 					</div>
 					<div class="list-item-button">
 						<BaseButton primary @click="$router.push('Badetemp')" style="margin: 1em 0 1em 0;">Prøv</BaseButton>
@@ -74,5 +78,6 @@
 
 	.list-item-button {
 		margin-left: 2em;
+		margin-top: 2em;
 	}
 </style>
