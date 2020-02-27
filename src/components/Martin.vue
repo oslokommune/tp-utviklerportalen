277217a63e6f05fd4295ca7965f2ba7b18791dee
Martin V. Løland <martvl@netcompany.com>
2020-02-27 11:46:25 +0100

    Slack integrasjon

--- src/components/Martin.vue
+++ src/components/Martin.vue
@@ -1,13 +1,35 @@
 <template>
-	<div class="Martin">
-
-	</div>
+    <div class="Martin">
+        <TextInput label="Jeg trenger hjelp!" v-model="message"></TextInput>
+        <br/>
+        <BaseButton v-on:click="post_to_slack">Send melding</BaseButton>
+    </div>
 </template>
 
 <script>
-export default {
-	name: 'Martin'
-}
+    import axios from 'axios'
+    import { BaseButton } from '@tuuturu/vue/buttons'
+    import { TextInput } from '@tuuturu/vue/forms'
+    export default {
+        name: 'Martin',
+        components: { BaseButton, TextInput },
+        data: () => ({
+            message: ''
+        }),
+        methods: {
+            post_to_slack: function (event) {
+                const url = 'https://hooks.slack.com/services/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'
+
+                axios.post(url, JSON.stringify({text: this.message}), {
+                    withCredentials: false,
+                    transformRequest: [(message, headers) => {
+                        delete headers.post["Content-Type"]
+                        return message
+                    }]
+                })
+            }
+        }
+    }
 </script>
 
 <style lang="scss" scoped>
